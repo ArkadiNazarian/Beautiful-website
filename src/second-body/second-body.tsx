335efcd6bb2341cbac6dcb6e8c2bc4546de6a2ad
ArkadiNazarian <nazarianarkadi@gmail.com>
2023-05-22 15:34:33 +0430

    starting the second body content

--- src/second-body/second-body.tsx
+++ src/second-body/second-body.tsx
@@ -5,11 +5,20 @@ export const SecondBody = () => {
 
     return (
         <div id="second-body" >
-            <img src={dessert} id="first-image" data-scroll data-scroll-speed="9" alt="first"/>
-            <div  data-scroll data-scroll-speed="2" >
-                <p id="planes">planes</p>
+            <div id="first-image-content" data-scroll data-scroll-speed="1">
+                <img src={dessert} id="first-image" alt="first" />
+                <p>QUÉ HACER</p>
+                <div id="first-let-img-first-line-word-underline"></div>
+                <p>Agenda Semanal. <br />Del 22 al 28 de <br />mayo</p>
             </div>
-            <img src={dessert} id="second-image" data-scroll data-scroll-speed="1" alt="second"/>
+            <div data-scroll data-scroll-speed="2" >
+                <div id="planes">
+                    <div id="first-line"></div>
+                    <p id="planes-word">PLANES</p>
+                    <div id="second-line"></div>
+                </div>
+            </div>
+            <img src={dessert} id="second-image" data-scroll data-scroll-speed="1" alt="second" />
         </div>
     )
 }
