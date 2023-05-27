7f7a53818724f60e7b4196ed1afe27e78eff3f2f
ArkadiNazarian <nazarianarkadi@gmail.com>
2023-05-27 10:33:07 +0430

    fix the fade in

--- src/second-body/second-body.tsx
+++ src/second-body/second-body.tsx
@@ -5,14 +5,28 @@ import React from 'react';
 const SecondBody = () => {
     return (
         <div id="second-body" >
-            <div id="first-image-content" data-scroll data-scroll-speed="1">
-                <div id="first-image-bg">
-                    <img src={dessert} id="first-image" alt="first" />
+            <div>
+                <div id="first-image-content" data-scroll data-scroll-speed="1">
+                    <div id="first-image-bg">
+                        <img src={dessert} className="first-image" alt="first" data-scroll data-scroll-class="fadeIn" data-scroll-repeat="true"/>
+                    </div>
+                    <p id='hover-underline-animation-1'>QUÉ HACER</p>
+                    <p id='hover-underline-animation-2'>Agenda Semanal.</p>
+                    <p id='hover-underline-animation-3'>Del 22 al 28 de</p>
+                    <p id='hover-underline-animation-4'>mayo</p>
+
                 </div>
-                <p id='hover-underline-animation-1'>QUÉ HACER</p>
-                <p id='hover-underline-animation-2'>Agenda Semanal.</p>
-                <p id='hover-underline-animation-3'>Del 22 al 28 de</p>
-                <p id='hover-underline-animation-4'>mayo</p>
+
+                {/* <div id="first-image-content" data-scroll data-scroll-speed="1">
+                    <div id="first-image-bg">
+                        <img src={dessert} id="first-image" alt="first" />
+                    </div>
+                    <p id='hover-underline-animation-1'>QUÉ HACER</p>
+                    <p id='hover-underline-animation-2'>Agenda Semanal.</p>
+                    <p id='hover-underline-animation-3'>Del 22 al 28 de</p>
+                    <p id='hover-underline-animation-4'>mayo</p>
+
+                </div> */}
             </div>
             <div data-scroll data-scroll-speed="2" >
                 <div id="planes">
@@ -21,16 +35,18 @@ const SecondBody = () => {
                     <div id="second-line"></div>
                 </div>
             </div>
-            <div id="second-image-content" data-scroll data-scroll-speed="1">
-                <div id="second-image-bg" >
-                    <img src={dessert} id="second-image" alt="second" />
+            <div>
+                <div id="second-image-content" data-scroll data-scroll-speed="1">
+                    <div id="second-image-bg" >
+                        <img src={dessert} id="second-image" alt="second" />
+                    </div>
+                    <p id='hover-underline-animation-5'>TENDENCIAS</p>
+                    <p id='hover-underline-animation-6'>IQOS lanza en</p>
+                    <p id='hover-underline-animation-7'>España su primera</p>
+                    <p id='hover-underline-animation-8'>colaboración con</p>
+                    <p id='hover-underline-animation-9'>la marca</p>
+                    <p id='hover-underline-animation-10'>japonesa kolor</p>
                 </div>
-                <p id='hover-underline-animation-5'>TENDENCIAS</p>
-                <p id='hover-underline-animation-6'>IQOS lanza en</p>
-                <p id='hover-underline-animation-7'>España su primera</p>
-                <p id='hover-underline-animation-8'>colaboración con</p>
-                <p id='hover-underline-animation-9'>la marca</p>
-                <p id='hover-underline-animation-10'>japonesa kolor</p>
             </div>
         </div>
     )
