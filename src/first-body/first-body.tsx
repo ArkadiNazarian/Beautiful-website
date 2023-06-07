efef2177093d957f779df87bbcb8239e78aa3e94
ArkadiNazarian <nazarianarkadi@gmail.com>
2023-06-07 11:59:06 +0430

    responsive-1

--- src/first-body/first-body.tsx
+++ src/first-body/first-body.tsx
@@ -5,13 +5,13 @@ export const FirstBody = () => {
 
     return (
         <div id="first-body" >
-            <p className='word' data-scroll data-scroll-speed="9" data-scroll-class="fadeIn" data-scroll-repeat="true">R</p>
-            <p className='word' data-scroll data-scroll-speed="9" data-scroll-class="fadeIn" data-scroll-repeat="true">E</p>
-            <p className='word' data-scroll data-scroll-speed="9" data-scroll-class="fadeIn" data-scroll-repeat="true">V</p>
-            <p className='word' data-scroll data-scroll-speed="9" data-scroll-class="fadeIn" data-scroll-repeat="true">I</p>
-            <p className='word' data-scroll data-scroll-speed="9" data-scroll-class="fadeIn" data-scroll-repeat="true">S</p>
-            <p className='word' data-scroll data-scroll-speed="9" data-scroll-class="fadeIn" data-scroll-repeat="true">T</p>
-            <p className='word' data-scroll data-scroll-speed="9" data-scroll-class="fadeIn" data-scroll-repeat="true">A</p>
+            <p className='word-1' data-scroll data-scroll-speed="9" data-scroll-class="fadeIn" data-scroll-repeat="true">R</p>
+            <p className='word-2' data-scroll data-scroll-speed="9" data-scroll-class="fadeIn" data-scroll-repeat="true">E</p>
+            <p className='word-3' data-scroll data-scroll-speed="9" data-scroll-class="fadeIn" data-scroll-repeat="true">V</p>
+            <p className='word-4' data-scroll data-scroll-speed="9" data-scroll-class="fadeIn" data-scroll-repeat="true">I</p>
+            <p className='word-5' data-scroll data-scroll-speed="9" data-scroll-class="fadeIn" data-scroll-repeat="true">S</p>
+            <p className='word-6' data-scroll data-scroll-speed="9" data-scroll-class="fadeIn" data-scroll-repeat="true">T</p>
+            <p className='word-7' data-scroll data-scroll-speed="9" data-scroll-class="fadeIn" data-scroll-repeat="true">A</p>
         </div>
     )
 }
